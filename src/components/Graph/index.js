8e338478ec9f7f4a27e9c73668f2964b80372e00
João Luis Baraky <jlbaraky@gmail.com>
2021-08-08 01:27:03 -0300

    Upgrade the graph component with color changing and a function that adds a node.

--- src/components/Graph/index.js
+++ src/components/Graph/index.js
@@ -1,28 +1,46 @@
 import Graph from "react-graph-vis";
-
+import {useState} from 'react';
 export function GraphComponent() {
-  const graph = {
-    nodes: [
-      { id: 1},
-      { id: 2},
-      { id: 3},
-      { id: 4},
-      { id: 5}
-    ],
-    edges: [
-      { from: 1, to: 2 },
-      { from: 1, to: 3 },
-      { from: 2, to: 4 },
-      { from: 2, to: 5 }
-    ]
-  };
+
+  const createNode = () => {
+    setState(({ counter, graph: { nodes, edges }, ...rest }) => {
+      const id = counter + 1;
+      return {
+        graph: {
+          nodes: [
+            ...nodes,
+            {id: id, color: "red"}
+          ],
+          edges: [
+            ...edges,
+            {from: 1, to: id}
+          ]
+        },
+        counter: id,
+        ...rest
+      }
+    });
+  }
+
+  const changeState = (id) => {
+    setState(({ graph, ...rest }) => {
+      const node = graph.nodes.find(node => {return node.id == id});
+      const color = "blue";
+      node.color = color;
+      return {
+        graph,
+        ...rest
+      }
+    });
+  }
+
 
   const options = {
     layout: {
-      hierarchical: false
+      hierarchical: false,
+      randomSeed: 10
     },
     edges: {
-      arrows: '',
       color: "#000000"
     },
     nodes: {
@@ -34,13 +52,39 @@ export function GraphComponent() {
     }
   };
 
-  const events = {
-    select: function(event) {
-      var { nodes, edges } = event;
+  const [state, setState] = useState({
+    counter: 5,
+    graph: {
+      nodes: [
+        { id: 1, color: "red" },
+        { id: 2, color: "red" },
+        { id: 3, color: "red" },
+        { id: 4, color: "red" },
+        { id: 5, color: "red" }
+      ],
+      edges: [
+        { from: 1, to: 2 },
+        { from: 1, to: 3 },
+        { from: 2, to: 4 },
+        { from: 2, to: 5 }
+      ]
+    },
+    events: {
+      selectNode: ({ nodes}) => {
+        changeState(nodes[0]);
+  
+      },
+      doubleClick: ({ pointer: { canvas } }) => {
+        createNode();
+      }
     }
-  };
+  })
+
+  const { graph, events } = state;
+
   return (
     <Graph
+    key = {Math.random()}
       graph={graph}
       options={options}
       events={events}
