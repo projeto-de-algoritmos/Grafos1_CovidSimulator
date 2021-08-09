dcc9ef2e0e2bf4c0d7f2ac194b28ac7b8fb55fd2
João Luis Baraky <jlbaraky@gmail.com>
2021-08-09 01:54:56 -0300

    Add contaminate function. Now each node has a color representing its probability of contamination.

--- src/components/Graph/index.js
+++ src/components/Graph/index.js
@@ -3,8 +3,27 @@ import {useState} from 'react';
 
 export function GraphComponent({isAdd}) {
 
+  const contaminate = (node_v, node_w) => {
+    let v = adj[node_v.id];
+    let w = adj[node_w.id];
+    console.log(`vprob: ${v.prob} wprob: ${w.prob}`);
+    let prob;
+    if (v.mask && w.mask) {
+      prob = 0.015;
+    } else if (v.mask && !w.mask) {
+      prob = 0.050;
+    } else if (!v.mask && w.mask) {
+      prob = 0.300;
+    } else {
+      prob = 0.900;
+    }
+    w.prob = v.prob * prob;
+    node_w.color = gradient[Math.round(w.prob/10)];
+    console.log(`wprob: ${w.prob} round: ${Math.round(w.prob/10)} color: ${node_w.color}`);
+  }
+
   const bfs = () => {
-    const nodes = graph.nodes.filter(node => {return node.color == "red"});
+    const nodes = graph.nodes.filter(node => {return (node.color == "red" || node.color == "pink")});
     for(var i = 0; i < nodes.length; i++) {
       let v = nodes[i];
 
@@ -15,18 +34,18 @@ export function GraphComponent({isAdd}) {
       s.push(v.id);
       while(s.length > 0){
         var w = s.pop();
-        graph.nodes[w].color = colors[graph.nodes[w].color];
         setState(({ graph, ...rest }) => {
           return {
             graph,
             ...rest
           }
         });
-        for(var j = 0; j < adj[w].length; j++) {
-          let u = adj[w][j];
+        for(var j = 0; j < adj[w].edges.length; j++) {
+          let u = adj[w].edges[j];
           if (!verified[u]) {
             s.push(u);
             verified[u] = true;
+            contaminate(graph.nodes[w], graph.nodes[u]);
           }
         }
       }
@@ -34,20 +53,20 @@ export function GraphComponent({isAdd}) {
     }
   }
   const adj = [
-    [1],
-    [0, 2],
-    [1, 3],
-    [2, 4],
-    [3, 5],
-    [4, 6],
-    [5, 7],
-    [6, 8],
-    [7, 9],
-    [8, 10],
-    [9, 11],
-    [10, 12],
-    [11, 13],
-    [12]
+    {mask: false, prob: 0.0, edges: [1]},
+    {mask: false, prob: 0.0, edges: [0, 2]},
+    {mask: false, prob: 0.0, edges: [1, 3]},
+    {mask: false, prob: 0.0, edges: [2, 4]},
+    {mask: false, prob: 0.0, edges: [3, 5]},
+    {mask: false, prob: 0.0, edges: [4, 6]},
+    {mask: false, prob: 0.0, edges: [5, 7]},
+    {mask: false, prob: 0.0, edges: [6, 8]},
+    {mask: false, prob: 0.0, edges: [7, 9]},
+    {mask: false, prob: 0.0, edges: [8, 10]},
+    {mask: false, prob: 0.0, edges: [9, 11]},
+    {mask: false, prob: 0.0, edges: [10, 12]},
+    {mask: false, prob: 0.0, edges: [11, 13]},
+    {mask: false, prob: 0.0, edges: [12]}
   ]
 
 
@@ -63,8 +82,8 @@ export function GraphComponent({isAdd}) {
   const buildEdges = () => {
     const edges = []
     for (var i = 0; i < adj.length; i++) {
-      for(var j = 0; j < adj[i].length; j++) {
-        edges.push({from: i, to: adj[i][j]});
+      for(var j = 0; j < adj[i].edges.length; j++) {
+        edges.push({from: i, to: adj[i].edges[j]});
       }
     }
     return edges;
@@ -77,10 +96,39 @@ export function GraphComponent({isAdd}) {
     "pink": "gray"
   }
 
+  const gradient = {
+    0: "#ffffff",
+    1: "#ffece5",
+    2: "#ffd9cb",
+    3: "#ffc6b2",
+    4: "#ffb299",
+    5: "#ff9e81",
+    6: "#ff8969",
+    7: "#ff7352",
+    8: "#ff5b3a",
+    9: "#ff3e22",
+    10: "#ff0000"
+  }
   const changeState = (id) => {
     const node = graph.nodes.find(node => {return node.id == id});
     const color = node.color;
     node.color = colors[color];
+
+    if (node.color == "red") {
+      adj[node.id].prob = 100.0;
+      adj[node.id].mask = false;
+    } else if (node.color == "pink") {
+      adj[node.id].prob = 100.0;
+      adj[node.id].mask = true;
+    } else if (node.color == "black") {
+      adj[node.id].prob = 0.0;
+      adj[node.id].mask = true;
+    }
+    else {
+      adj[node.id].prob = 0.0;
+      adj[node.id].mask = false;
+    }
+
     setState(({ graph, ...rest }) => {
       return {
         graph,
