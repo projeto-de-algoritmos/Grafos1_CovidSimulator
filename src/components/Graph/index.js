9bc3d302f88506515a234d7dc89d89a804ef1dda
João Luis Baraky <jlbaraky@gmail.com>
2021-08-09 16:17:10 -0300

    Update graph.

--- src/components/Graph/index.js
+++ src/components/Graph/index.js
@@ -17,6 +17,7 @@ export function GraphComponent({isAdd}) {
       prob = 0.900;
     }
     w.prob = (1 - (1 - (v.prob*prob)/100) * (1 - w.prob/100))*100;
+    console.log(`v(${node_v.id}) prob: ${v.prob} | w(${node_w.id}) prob: ${w.prob}`)
     node_w.color = gradient[Math.round(w.prob/10)];
   }
 
@@ -32,8 +33,10 @@ export function GraphComponent({isAdd}) {
       s.push(v.id);
       v.color = gradient[10];
 
+      console.log(v.id);
+
       while(s.length > 0){
-        var w = s.pop();
+        var w = s.shift();
 
         for(var j = 0; j < adj[w].edges.length; j++) {
           let u = adj[w].edges[j];
@@ -57,31 +60,31 @@ export function GraphComponent({isAdd}) {
     }
   }
   const adj = [
-    {mask: false, prob: 0.0, edges: [1, 7, 12, 14, 23, 7]}, //0
-    {mask: false, prob: 0.0, edges: [0, 2, 4, 13]}, // 1
-    {mask: false, prob: 0.0, edges: [1, 3, 17, 10, 22]},// 2
-    {mask: false, prob: 0.0, edges: [2, 4, 6, 14]},// 3
-    {mask: false, prob: 0.0, edges: [1, 3, 5, 9, 16]}, // 4
-    {mask: false, prob: 0.0, edges: [4, 6, 18, 21]}, // 5
-    {mask: false, prob: 0.0, edges: [5, 7, 3, 11, 18, 24]}, // 6
-    {mask: false, prob: 0.0, edges: [0, 6, 8, 15]}, // 7
+    {mask: false, prob: 0.0, edges: [1, 14, 7]}, //0
+    {mask: false, prob: 0.0, edges: [0, 2, 13]}, // 1
+    {mask: false, prob: 0.0, edges: [1, 10, 22]},// 2
+    {mask: false, prob: 0.0, edges: [4, 6, 14, 15]},// 3
+    {mask: false, prob: 0.0, edges: [3, 5, 16]}, // 4
+    {mask: false, prob: 0.0, edges: [4,18, 21]}, // 5
+    {mask: false, prob: 0.0, edges: [7, 3, 24]}, // 6
+    {mask: false, prob: 0.0, edges: [6, 8, 15]}, // 7
     {mask: false, prob: 0.0, edges: [7, 9, 20]}, // 8
-    {mask: false, prob: 0.0, edges: [8, 10, 4, 17, 19]}, // 9
-    {mask: false, prob: 0.0, edges: [2, 9, 11, 22, 26, 25]}, // 10
-    {mask: false, prob: 0.0, edges: [10, 12, 6, 17, 23]}, // 11
-    {mask: false, prob: 0.0, edges: [0, 11, 13, 24]}, // 12
+    {mask: false, prob: 0.0, edges: [8, 17, 19]}, // 9
+    {mask: false, prob: 0.0, edges: [2, 26, 25]}, // 10
+    {mask: false, prob: 0.0, edges: [12, 17, 23]}, // 11
+    {mask: false, prob: 0.0, edges: [11, 13, 24]}, // 12
     {mask: false, prob: 0.0, edges: [1, 12]}, // 13
-    {mask: false, prob: 0.0, edges: [0, 3, 23, 15, 16, 23]}, // 14
-    {mask: false, prob: 0.0, edges: [14, 7, 16, 17, 18, 22, 26]}, // 15
-    {mask: false, prob: 0.0, edges: [15, 4, 14, 17]}, // 16
-    {mask: false, prob: 0.0, edges: [2, 11, 16, 18, 9, 15, 23]}, // 17
-    {mask: false, prob: 0.0, edges: [5, 15, 17, 6, 19]}, // 18
+    {mask: false, prob: 0.0, edges: [0, 3, 16]}, // 14
+    {mask: false, prob: 0.0, edges: [3, 7, 18, 26]}, // 15
+    {mask: false, prob: 0.0, edges: [4, 14, 17]}, // 16
+    {mask: false, prob: 0.0, edges: [11, 16, 9]}, // 17
+    {mask: false, prob: 0.0, edges: [5, 15, 19]}, // 18
     {mask: false, prob: 0.0, edges: [9, 18, 21]}, // 19
     {mask: false, prob: 0.0, edges: [8, 21]}, // 20
-    {mask: false, prob: 0.0, edges: [20, 5, 19, 22]}, // 21
-    {mask: false, prob: 0.0, edges: [21, 2, 15, 10, 23]}, // 22
-    {mask: false, prob: 0.0, edges: [0, 22, 11, 14, 17, 24]}, // 23
-    {mask: false, prob: 0.0, edges: [6, 23, 12, 25]}, // 24
+    {mask: false, prob: 0.0, edges: [20, 5, 19]}, // 21
+    {mask: false, prob: 0.0, edges: [2, 23]}, // 22
+    {mask: false, prob: 0.0, edges: [22, 11]}, // 23
+    {mask: false, prob: 0.0, edges: [6, 12, 25]}, // 24
     {mask: false, prob: 0.0, edges: [10, 24]}, // 25
     {mask: false, prob: 0.0, edges: [10, 15]} // 26
   ]
@@ -90,7 +93,7 @@ export function GraphComponent({isAdd}) {
   const buildNodes = () => {
     const nodes = []
     for (var i = 0; i < adj.length; i++) {
-      nodes.push({id: i, color: "gray"});
+      nodes.push({id: i, color: "gray", lable: "a"});
     }
     return nodes;
    
